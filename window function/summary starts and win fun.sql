aa25ea6e0b6cddaa4345434f27c54aa07ab0a57a
SyroidNadiia <shcherbelova@ukr.net>
2025-01-29 20:42:48 +0200

    exercises on absolute and relative functions are solved

--- window function/summary starts and win fun.sql
+++ window function/summary starts and win fun.sql
@@ -143,3 +143,63 @@ SELECT
             ORDER BY Year ASC) AS Last_Champion
 FROM Athletics_Gold
 ORDER BY Event ASC, Gender ASC, Year ASC;
+
+-- For each year, fetch the current gold medalist and the gold medalist 3 competitions ahead of the current row.
+
+WITH Discus_Medalists AS (
+  SELECT DISTINCT
+    Year,
+    Athlete
+  FROM Summer_Medals
+  WHERE Medal = 'Gold'
+    AND Event = 'Discus Throw'
+    AND Gender = 'Women'
+    AND Year >= 2000)
+
+SELECT
+  -- For each year, fetch the current and future medalists
+  year,
+  athlete,
+  LEAD(athlete, 3) OVER (ORDER BY year ASC) AS Future_Champion
+FROM Discus_Medalists
+ORDER BY Year ASC;
+
+-- Return all athletes and the first athlete ordered by alphabetical order.
+
+WITH All_Male_Medalists AS (
+  SELECT DISTINCT
+    Athlete
+  FROM Summer_Medals
+  WHERE Medal = 'Gold'
+    AND Gender = 'Men')
+
+SELECT
+  -- Fetch all athletes and the first athlete alphabetically
+  Athlete,
+  FIRST_VALUE(athlete) OVER (
+    ORDER BY athlete ASC
+  ) AS First_Athlete
+FROM All_Male_Medalists;
+
+-- Last country by name
+-- Just like you can get the first row's value in a dataset, you can get the last row's value. This is often useful when you want to compare the most recent value to previous values.
+
+-- Return the year and the city in which each Olympic games were held.
+-- Fetch the last city in which the Olympic games were held.
+
+WITH Hosts AS (
+  SELECT DISTINCT Year, City
+    FROM Summer_Medals)
+
+SELECT
+  Year,
+  City,
+  -- Get the last city in which the Olympic games were held
+  LAST_VALUE(city) OVER (
+   ORDER BY year ASC
+   RANGE BETWEEN
+     UNBOUNDED PRECEDING AND
+     UNBOUNDED FOLLOWING
+  ) AS Last_City
+FROM Hosts
+ORDER BY Year ASC;
