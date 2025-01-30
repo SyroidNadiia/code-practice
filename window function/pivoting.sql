e6acc0f96f8a1eee744e576297d7d1d9bb71aeb2
SyroidNadiia <shcherbelova@ukr.net>
2025-01-30 20:15:59 +0200

    added a few exercises about pivoting

---
+++ window function/pivoting.sql
@@ -0,0 +1,22 @@
+-- Create the correct extension.
+-- Fill in the column names of the pivoted table.
+
+-- Create the correct extension to enable CROSSTAB
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  SELECT
+    Gender, Year, Country
+  FROM Summer_Medals
+  WHERE
+    Year IN (2008, 2012)
+    AND Medal = 'Gold'
+    AND Event = 'Pole Vault'
+  ORDER By Gender ASC, Year ASC;
+-- Fill in the correct column names for the pivoted table
+$$) AS ct (gender VARCHAR,
+           "2008" VARCHAR,
+           "2012" VARCHAR)
+
+ORDER BY Gender ASC;
+
