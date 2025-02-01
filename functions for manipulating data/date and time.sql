9a033d8ec31c7e3e92eaadb6b6c5b8e41bde2152
SyroidNadiia <shcherbelova@ukr.net>
2025-02-01 20:45:25 +0200

    solved exercises with date and time

--- functions for manipulating data/date and time.sql
+++ functions for manipulating data/date and time.sql
@@ -6,3 +6,152 @@ SELECT
 	rental_date + INTERVAL '3 days' AS expected_return_date
 FROM rental;
 
+-- Subtract the rental_date from the return_date to calculate the number of days_rented.
+
+SELECT f.title, f.rental_duration,
+    -- Calculate the number of days rented
+    r.return_date - r.rental_date AS days_rented
+FROM film AS f
+     INNER JOIN inventory AS i ON f.film_id = i.film_id
+     INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+SELECT f.title, f.rental_duration,
+    -- Calculate the number of days rented
+	AGE(return_date, rental_date) AS days_rented
+FROM film AS f
+	INNER JOIN inventory AS i ON f.film_id = i.film_id
+	INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+-- Convert rental_duration by multiplying it with a 1 day INTERVAL
+-- Subtract the rental_date from the return_date to calculate the number of days_rented.
+-- Exclude rentals with a NULL value for return_date.
+
+SELECT
+	f.title,
+ 	-- Convert the rental_duration to an interval
+    INTERVAL '1' day * f.rental_duration,
+ 	-- Calculate the days rented as we did previously
+    r.return_date - r.rental_date AS days_rented
+FROM film AS f
+    INNER JOIN inventory AS i ON f.film_id = i.film_id
+    INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+-- Filter the query to exclude outstanding rentals
+WHERE r.return_date IS NOT NULL
+ORDER BY f.title;
+
+-- Convert rental_duration by multiplying it with a 1-day INTERVAL.
+-- Add it to the rental date.
+SELECT
+    f.title,
+	r.rental_date,
+    f.rental_duration,
+    -- Add the rental duration to the rental date
+    INTERVAL '1' day * f.rental_duration + r.rental_date AS expected_return_date,
+    r.return_date
+FROM film AS f
+    INNER JOIN inventory AS i ON f.film_id = i.film_id
+    INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+
+-- Select the current timestamp
+SELECT NOW();
+-- 2025-02-01 18:59:07.769884
+
+-- Select the current date
+SELECT CURRENT_DATE;
+-- 2025-02-01
+
+--Select the current timestamp without a timezone
+SELECT CAST( NOW() AS timestamp )
+2025-02-01 18:59:59.100525
+
+SELECT 
+	-- Select the current date
+	CURRENT_DATE,
+    -- CAST the result of the NOW() function to a date
+    CAST( NOW() AS date )
+--    2025-02-01	2025-02-01 
+
+--Select the current timestamp without timezone
+SELECT  CURRENT_TIMESTAMP::timestamp AS right_now;
+-- 2025-02-01 19:03:34.586451
+
+-- Now select a timestamp five days from now and alias it as five_days_from_now.
+SELECT
+	CURRENT_TIMESTAMP::timestamp AS right_now,
+    INTERVAL '5 days' + CURRENT_TIMESTAMP AS five_days_from_now;
+-- Finally, let's use a second-level precision with no fractional digits for both the right_now and five_days_from_now fields.
+    SELECT
+	CURRENT_TIMESTAMP(0)::timestamp AS right_now,
+    interval '5 days' + CURRENT_TIMESTAMP(0) AS five_days_from_now;
+
+-- - Get the day of the week from the rental_date column.
+SELECT 
+  -- Extract day of week from rental_date
+  EXTRACT(dow from rental_date) AS dayofweek 
+FROM rental 
+LIMIT 100;
+-- dow - витягує номер дня тижня (0-6)/ де 0 = неділя
+-- EXTRACT(day FROM date) - День у місяці (1-31)
+-- TO_CHAR(date, 'Day') - Назва дня тижня (Monday, Tuesday...)
+
+-- Truncate rental_date by year
+SELECT DATE_TRUNC('year', rental_date) AS rental_year
+FROM rental;
+
+-- Truncate rental_date by month
+SELECT DATE_TRUNC('month', rental_date) AS rental_month
+FROM rental;
+-- Truncate rental_date by day of the month 
+SELECT DATE_TRUNC('day', rental_date) AS rental_day 
+FROM rental;
+
+
+SELECT 
+  DATE_TRUNC('day', rental_date) AS rental_day,
+  -- Count total number of rentals 
+  COUNT(*) AS rentals 
+FROM rental
+GROUP BY 1;
+
+-- Extract the day of the week from the rental_date column using the alias dayofweek.
+-- Use an INTERVAL in the WHERE clause to select records for the 90 day period starting on 5/1/2005.
+SELECT 
+  -- Extract the day of week date part from the rental_date
+  EXTRACT(day from rental_date) AS dayofweek,
+  AGE(return_date, rental_date) AS rental_days
+FROM rental AS r 
+WHERE 
+  -- Use an INTERVAL for the upper bound of the rental_date 
+  rental_date BETWEEN CAST('2005-05-01' AS DATE)
+   AND CAST('2005-05-01' AS DATE) + INTERVAL '90 day';
+
+-- Finally, use a CASE statement and DATE_TRUNC() to create a new column called past_due which will be TRUE if the rental_days is greater than the rental_duration otherwise, it will be FALSE.
+SELECT 
+  c.first_name || ' ' || c.last_name AS customer_name,
+  f.title,
+  r.rental_date,
+  -- Extract the day of week date part from the rental_date
+  EXTRACT(dow FROM r.rental_date) AS dayofweek,
+  AGE(r.return_date, r.rental_date) AS rental_days,
+  -- Use DATE_TRUNC to get days from the AGE function
+  CASE WHEN DATE_TRUNC('day', AGE(r.return_date, r.rental_date)) > 
+  -- Calculate number of d
+    f.rental_duration * INTERVAL '1' day 
+  THEN TRUE 
+  ELSE FALSE END AS past_due 
+FROM 
+  film AS f 
+  INNER JOIN inventory AS i 
+  	ON f.film_id = i.film_id 
+  INNER JOIN rental AS r 
+  	ON i.inventory_id = r.inventory_id 
+  INNER JOIN customer AS c 
+  	ON c.customer_id = r.customer_id 
+WHERE 
+  -- Use an INTERVAL for the upper bound of the rental_date 
+  r.rental_date BETWEEN CAST('2005-05-01' AS DATE) 
+  AND CAST('2005-05-01' AS DATE) + INTERVAL '90 day';
