1225ffc7de73e1ec6da97cf5c72fc12818875370
SyroidNadiia <shcherbelova@ukr.net>
2025-02-19 11:14:30 +0200

    solved a few exersises with create a new table for the non-key comumns, drop columns

--- Database design/Database_design.sql
+++ Database design/Database_design.sql
@@ -79,3 +79,102 @@ WHERE
     dim_book_star.genre = 'novel'
 -- Group results by state
 GROUP BY dim_store_star.state;
+
+
+-- Select state from the appropriate table and the total sales_amount.
+-- Complete the two JOINS to get the genre_id's.
+-- Complete the three JOINS to get the state_id's.
+-- Conditionally select for books with the genre novel.
+-- Group the results by state.
+-- Output each state and their total sales_amount
+SELECT dim_state_sf.state, SUM(sales_amount)
+FROM fact_booksales
+    -- Joins for genre
+    JOIN dim_book_sf on dim_book_sf.book_id = fact_booksales.book_id
+    JOIN dim_genre_sf on dim_book_sf.genre_id = dim_genre_sf.genre_id
+    -- Joins for state 
+    JOIN dim_store_sf on dim_store_sf.store_id = fact_booksales.store_id 
+    JOIN dim_city_sf on dim_store_sf.city_id = dim_city_sf.city_id
+	JOIN dim_state_sf on  dim_city_sf.state_id = dim_state_sf.state_id
+-- Get all books with in the novel genre and group the results by state
+WHERE  
+    dim_genre_sf.genre = 'novel'
+GROUP BY
+    dim_state_sf.state;
+
+
+--     Add a continent_id column to dim_country_sf with a default value of 1. Note thatNOT NULL DEFAULT(1) constrains a value from being null and defaults its value to 1.
+-- Make that new column a foreign key reference to dim_continent_sf's continent_id.
+
+-- Add a continent_id column with default value of 1
+ALTER TABLE dim_country_sf
+ADD continent_id int NOT NULL DEFAULT(1);
+
+-- Add the foreign key constraint
+ALTER TABLE dim_country_sf ADD CONSTRAINT country_continent
+   FOREIGN KEY (continent_id) REFERENCES dim_continent_sf(continent_id);
+   
+-- Output updated table
+SELECT * FROM dim_country_sf;
+
+
+
+-- cars_rented holds one or more car_ids and invoice_id holds multiple values. Create a new table to hold individual car_ids and invoice_ids of the customer_ids who've rented those cars.
+-- Drop two columns from customers table to satisfy 1NF“
+-- Create a new table to hold the cars rented by customers
+CREATE TABLE cust_rentals (
+  customer_id INT NOT NULL,
+  car_id VARCHAR(128) NULL,
+  invoice_id VARCHAR(128) NULL
+);
+-- Drop two columns from customers table to satisfy 1NF
+ALTER TABLE customers
+DROP COLUMN cars_rented,
+DROP COLUMN invoice_id;
+
+
+-- Create a new table for the non-key columns that were conflicting with 2NF criteria.
+-- Drop those non-key columns from customer_rentals.
+-- Create a new table to satisfy 2NF
+CREATE TABLE cars (
+  car_id VARCHAR(256) NULL,
+  model VARCHAR(128),
+  manufacturer VARCHAR(128),
+  type_car VARCHAR(128),
+  condition VARCHAR(128),
+  color VARCHAR(128)
+);
+
+-- Insert data into the new table
+INSERT INTO cars
+SELECT DISTINCT
+  car_id,
+  model,
+  manufacturer,
+  type_car,
+  condition,
+  color
+FROM customer_rentals;
+
+-- Drop columns in customer_rentals to satisfy 2NF
+ALTER TABLE customer_rentals
+DROP COLUMN model,
+DROP COLUMN manufacturer, 
+DROP COLUMN type_car,
+DROP COLUMN condition,
+DROP COLUMN color;
+
+
+
+-- Create a new table for the non-key columns that were conflicting with 3NF criteria.
+-- Drop those non-key columns from rental_cars.
+-- Create a new table to satisfy 3NF
+CREATE TABLE car_model(
+  model VARCHAR(128),
+  manufacturer VARCHAR(128),
+  type_car VARCHAR(128)
+);
+-- Drop columns in rental_cars to satisfy 3NF
+ALTER TABLE rental_cars
+DROP COLUMN manufacturer, 
+DROP COLUMN type_car;
