462dd9ff378a7f1fe6b3cc528ca53a4b38442715
SyroidNadiia <shcherbelova@ukr.net>
2025-02-20 12:02:35 +0200

    exercises on granting access rights, adding to the user group have been solved

--- Database design/Database_design.sql
+++ Database design/Database_design.sql
@@ -178,3 +178,135 @@ CREATE TABLE car_model(
 ALTER TABLE rental_cars
 DROP COLUMN manufacturer, 
 DROP COLUMN type_car;
+
+
+
+-- Query the information schema to get views.
+-- Exclude system views in the results.
+-- Get all non-systems views
+SELECT * FROM information_schema.views
+WHERE table_schema NOT IN ('pg_catalog', 'information_schema');
+
+
+
+-- Create a view called high_scores that holds reviews with scores above a 9.
+CREATE view high_scores AS
+SELECT * FROM reviews
+WHERE score > 9;
+-- Count the number of self-released works in high_scores
+SELECT COUNT(*) FROM high_scores
+INNER JOIN labels ON labels.reviewid = high_scores.reviewid
+WHERE label = 'self-released';
+
+
+
+-- Create a view called top_artists_2017 with artist from artist_title.
+-- To only return the highest scoring artists of 2017, join the views top_15_2017 and artist_title on reviewid.
+-- Output top_artists_2017.
+-- Create a view with the top artists in 2017
+CREATE VIEW top_artists_2017 AS
+-- with only one column holding the artist field
+SELECT artist_title.artist FROM top_15_2017
+INNER JOIN artist_title
+ON artist_title.reviewid = top_15_2017.reviewid;
+
+-- Output the new view
+SELECT * FROM top_artists_2017;
+
+
+-- Revoke all database users' update and insert privileges on the long_reviews view.
+-- Grant the editor user update and insert privileges on the long_reviews view.
+-- Revoke everyone's update and insert privileges
+REVOKE UPDATE, INSERT ON long_reviews FROM public; 
+-- Grant the editor update and insert privileges 
+GRANT UPDATE, INSERT ON long_reviews TO editor; 
+
+
+
+-- Use CREATE OR REPLACE to redefine the artist_title view.
+-- Respecting artist_title's original columns of reviewid, title, and artist, add a label column from the labels table.
+-- Join the labels table using the reviewid field.
+
+-- Redefine the artist_title view to have a label column
+CREATE OR REPLACE VIEW artist_title AS
+SELECT reviews.reviewid, reviews.title, artists.artist, labels.label
+FROM reviews
+INNER JOIN artists
+ON artists.reviewid = reviews.reviewid
+INNER JOIN labels
+ON labels.reviewid = artists.reviewid;
+
+SELECT * FROM artist_title;
+
+
+
+-- Create a materialized view called genre_count that holds the number of reviews for each genre.
+-- Refresh genre_count so that the view is up-to-date.
+-- Create a materialized view called genre_count 
+CREATE MATERIALIZED VIEW genre_count AS
+SELECT genre, COUNT(*) 
+FROM genres
+GROUP BY genre;
+INSERT INTO genres
+VALUES (50000, 'classical');
+-- Refresh genre_count
+REFRESH MATERIALIZED VIEW genre_count;
+SELECT * FROM genre_count;
+
+
+
+-- Create a data scientist role
+CREATE ROLE data_scientist;
+-- Create a role for Marta
+CREATE ROLE marta LOGIN;
+
+-- Create a role called admin with the ability to create databases (CREATEDB) and to create roles (CREATEROLE).
+CREATE ROLE admin WITH CREATEDB CREATEROLE;
+
+
+
+
+-- Grant data_scientist update and insert privileges
+GRANT UPDATE, INSERT ON long_reviews TO data_scientist;
+
+-- Give Marta's role a password
+ALTER ROLE marta WITH password 's3cur3p@ssw0rd';
+
+
+
+-- Add Marta to the data scientist group
+GRANT data_scientist TO marta;
+-- Celebrate! You hired data scientists.
+-- Remove Marta from the data scientist group
+REVOKE data_scientist FROM marta;
+
+
+
+-- Create a new table called film_descriptions
+CREATE TABLE film_descriptions (
+    film_id INT,
+    long_description TEXT
+);
+
+-- Copy the descriptions from the film table
+INSERT INTO film_descriptions
+SELECT film_id, long_description FROM film;
+
+
+
+-- Create a new table called film_descriptions
+CREATE TABLE film_descriptions (
+    film_id INT,
+    long_description TEXT
+);
+
+-- Copy the descriptions from the film table
+INSERT INTO film_descriptions
+SELECT film_id, long_description FROM film;
+    
+-- Drop the descriptions from the original table
+ALTER TABLE film DROP COLUMN long_description;
+
+-- Join to view the original table
+SELECT * FROM film 
+JOIN film_descriptions USING(film_id);
